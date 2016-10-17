a9db8f40e1b7af95bf32ea0ccfc964f76f30c3ab
Sunil Patel <sunil.patel@hanusoftware.local>
2016-10-17 11:13:26 +0530

    abc
    test

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevopWithJenkinsUI.Startup))]
+namespace DevopWithJenkinsUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevopWithJenkinsUI.MvcApplication" Language="C#" %>
